6a8de37d933df1d69173828915a3e7043da30b4c
jorchsg <sandoval.jorge.407@gmail.com>
2021-04-28 23:42:27 -0700

    initialize project

---
+++ src/views/Home.vue
@@ -0,0 +1,138 @@
+<template>
+  <!-- Input Text -->
+  <form @submit.prevent="procesarFormulario">
+    <input 
+      class="form-control my-2"
+      placeholder="Ingrese Nombre"
+      v-model.trim="tarea.nombre"
+    >
+  </form>
+
+  <!-- Input Checkkbox -->
+  <div class="my-2">
+    <div class="form-check form-check-inline">
+      <input 
+        type="checkbox"
+        class="form-check-input"
+        id="check-1"
+        v-model="tarea.categoria"
+        value="Javascript"
+      >
+      <label 
+        for="check-1"
+        class="form-check-label"
+      >Javascript</label>
+    </div>
+    <div class="form-check form-check-inline">
+      <input 
+        type="checkbox"
+        class="form-check-input"
+        id="check-2"
+        v-model="tarea.categoria"
+        value="Desarrollo"
+      >
+      <label 
+        for="check-2"
+        class="form-check-label"
+      >Desarrollo web</label>
+    </div>
+  </div>
+
+  <!-- radio -->
+  <div class="my-2">
+    <div class="form-check form-check-inline">
+      <input 
+        class="form-check-input" 
+        type="radio" 
+        id="inlineRadio1" 
+        value="urgente"
+        v-model="tarea.estado"
+      >
+      <label class="form-check-label" for="inlineRadio1">
+        Urgente
+      </label>
+    </div>
+
+    <div class="form-check form-check-inline">
+      <input 
+        class="form-check-input" 
+        type="radio" 
+        id="inlineRadio2" 
+        value="relax"
+        v-model="tarea.estado"
+      >
+      <label class="form-check-label" for="inlineRadio2">
+        Relax
+      </label>
+    </div>
+  </div>
+
+  <!-- Numero -->
+  <div class="mt-2">
+    <input 
+      type="number"
+      class="form-control"
+      v-model.number="tarea.numero"
+    >
+  </div>
+
+  <button 
+    class="btn btn-block my-3 btn-dark" 
+    @click="procesarFormulario"
+    :disabled="bloquear"
+  >
+    Procesar Formulario
+  </button>
+
+  <div>
+    <p>{{mensaje}}</p>
+  </div>
+
+  <div>
+    <p>{{tarea}}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+    
+  },
+  data(){
+    return {
+      tarea: {
+        nombre: '',
+        categoria: [],
+        estado: '',
+        numero: 0
+      },
+      mensaje : ''
+    }
+  },
+  methods: {
+    procesarFormulario(){
+      console.log(this.tarea)
+      if(this.tarea.nombre.trim() === ''){
+        this.mensaje= 'Favor de Llenar el Formulario'
+        return
+      }else{
+        this.mensaje = "Formulario Enviado con Éxito"
+        // Limpiar Formulario
+        this.tarea = {
+          nombre: '',
+          categoria: [],
+          estado: '',
+          numero: 0
+        };
+      }      
+    }
+  },
+  computed:{
+    bloquear(){
+      return this.tarea.nombre.trim() === "" ? true : false;
+    },
+  }
+}
+</script>
